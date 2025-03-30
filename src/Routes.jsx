3690224f82812b458b433f6f5e3dddb52716130c
ZichengHe-Susan <bgc4bx@virginia.edu>
2025-03-29 21:58:03 -0400

    added plan page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD:src/App.jsx
-=======
 import Login from './Login';
->>>>>>> main:src/Routes.jsx
 import HomePage from './Homepage';
 import AddTourPage from './AddTourPage';
 import ViewTourPlan from './ViewTourPlan';
@@ -12,12 +9,8 @@ const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD:src/App.jsx
-        <Route path="/" element={<HomePage />} />
-=======
         <Route path="/" element={<Login />} />
         <Route path="/homepage" element={<HomePage />} />
->>>>>>> main:src/Routes.jsx
         <Route path="/add-tour" element={<AddTourPage />} />
         <Route path="/view-tour-plan" element={<ViewTourPlan />} />
       </Routes>
